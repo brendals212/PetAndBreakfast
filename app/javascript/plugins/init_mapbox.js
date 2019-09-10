67bc82cf156c0b4c6edeee442ca9560db91a590c
Gilad Sarusi <giladsar@gmail.com>
2019-09-10 17:09:48 +0300

    Add stylesheet pack tag

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -6,8 +6,6 @@ const initMapbox = () => {
   const addMarkersToMap = (map, markers) => {
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-      console.log(marker.infoWindow);
-
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup) // add this
