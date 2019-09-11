1a6be19ab910bde5fc99b2adc5928f7820b19dd4
Gilad Sarusi <giladsar@gmail.com>
2019-09-11 15:44:15 +0300

    done profile with edit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,4 +3,20 @@ class DashboardController < ApplicationController
     @user = User.find(params[:id])
     @pets = @user.pets
   end
+
+  def edit
+    @user = current_user
+    @pets = @user.pets
+  end
+
+  def update
+    current_user.update(set_params)
+    redirect_to profile_dashboard_path(current_user)
+  end
+
+  private
+
+  def set_params
+    params.require(:user).permit(:email, :description, :photo, :address)
+  end
 end
