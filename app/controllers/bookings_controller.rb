a45102828e52c4087ea92425496c467775cff957
Brenda Sanchez <brendals212@gmail.com>
2019-09-11 18:21:27 +0300

    Calculation for Calendar done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,8 +8,8 @@ class BookingsController < ApplicationController
   def new
     @pet = Pet.find(params[:pet_id])
     @owner = @pet.user
-    @start_date = Date.new(params[:dates]["starts_at(1i)"].to_i, params[:dates]["starts_at(2i)"].to_i, params[:dates]["starts_at(3i)"].to_i)
-    @end_date = Date.new(params[:dates]["ends_at(1i)"].to_i, params[:dates]["ends_at(2i)"].to_i, params[:dates]["ends_at(3i)"].to_i)
+    @start_date = Date.new(params[:dates][:starts_at][0..3].to_i, params[:dates][:starts_at][5..6].to_i, params[:dates][:starts_at][8..9].to_i)
+    @end_date = Date.new(params[:dates][:starts_at][14..17].to_i, params[:dates][:starts_at][19..20].to_i, params[:dates][:starts_at][22..23].to_i)
     @days = (@end_date - @start_date)
     @cost = ((@pet.price/7.0) * @days).round(2)
     @booking = Booking.new
